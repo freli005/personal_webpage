84d72d057025fd6c4de6a6a499cfae8cf80b4983
Fredrik Lindsten <fredrik.lindsten@liu.se>
2019-12-28 20:26:50 +0100

    Added toggle feature in publication list using JS

---
+++ js/publications.js
@@ -0,0 +1,8 @@
+// Collapse all expandable texts in publication list when expanding a
+// new element in the same group (i.e. for the same publication)
+
+var $pubList = $('#publicationList');
+$pubList.on('show.bs.collapse','.collapse', function(e) {
+    var $myGroup = $('#' + e.target.parentNode.id);
+    $myGroup.find('.collapse.in').collapse('hide');
+});
